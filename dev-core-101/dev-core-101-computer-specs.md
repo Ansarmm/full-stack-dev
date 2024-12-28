5ee4529c50e46b72cc4af1730ef0e42068e99031
Ansarmm <issayevansar1@gmail.com>
2024-12-29 00:35:38 +0500

    Delete dev-core-101 directory

--- dev-core-101/dev-core-101-computer-specs.md
+++
@@ -1,4 +0,0 @@
-1. CPU - Apple M3
-2. RAM - 16Gb
-3. HD(ROM) - 494Gb
-4. OS - macOS
--- dev-core-101/dev-core-101-variables-python.py
+++
@@ -1,8 +0,0 @@
-x = 25
-y = 8
-print (x + y) #33
-print (x - y) #17
-
-name = "Ansar"
-greeting = "Hi! My name is " + name
-print(greeting)
