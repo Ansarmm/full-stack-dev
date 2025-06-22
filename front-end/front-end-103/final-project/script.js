5f18316a29f32e6ca9dbcbc1fa1bf47c4110e5e0
Ansar <issayevansar1@gmail.com>
2025-06-22 00:08:47 -0500

    Final project add structure and divide on page

---
+++ front-end/front-end-103/final-project/script.js
@@ -0,0 +1,19 @@
+// Scripts for navigation:
+//dark-theme
+const darktoggle = document.getElementById('dark')
+
+darktoggle.addEventListener('click', () => {
+    document.documentElement.classList.toggle('dark-theme')
+})
+
+// Highlight current page
+const navLinks = document.querySelectorAll(".navigation a"); 
+const currentUrl = window.location.pathname;
+
+navLinks.forEach(link => {
+  const linkHref = link.getAttribute("href");
+
+  if (linkHref === currentUrl || currentUrl.endsWith(linkHref)) {
+    link.classList.add("active");
+  }
+});
